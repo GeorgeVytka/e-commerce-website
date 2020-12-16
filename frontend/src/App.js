214dbef2170cbed0d30330ae5d5917da1b157641
George <finblack20@gmail.com>
2020-12-16 17:08:58 -0500

    implament react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './componets/Header';
 import Footer from './componets/Footer';
 import HomeScreen from './screens/HomeScreens';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
   return (
-    <>
+
+    <Router>
       <Header />
 
       <main className='py-3'>
         <Container>
           <h1>Welcome to Proshop</h1>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
 
 
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
